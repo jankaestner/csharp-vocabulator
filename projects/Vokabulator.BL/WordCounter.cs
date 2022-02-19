15a65d38a481078f2f5afc67c8f17e18547784a3
Julius Freiny <julius.freiny@gmx.de>
2022-02-19 12:43:10 +0100

    Implemented WordCounter with example

---
+++ projects/Vokabulator.BL/WordCounter.cs
@@ -0,0 +1,39 @@
+﻿namespace Vokabulator.BL
+{
+  public class WordCounter
+  {
+    private readonly char seperator;
+
+    public WordCounter(char seperator)
+    {
+      this.seperator = seperator;
+    }
+
+    //Zählen
+    public Dictionary<string, int> Count(string content)
+    {
+      Dictionary<string, int> countDictionary = new Dictionary<string,int>();
+
+      // Wörter auftrennen
+      string[] words = content.Split(seperator);
+      // für jedes Wort:
+      foreach (string word in words)
+      {
+        // schauen, ob es im Dictionary vorhanden ist
+        if (countDictionary.ContainsKey(word))
+        {
+          // wenn ja, dann Zähler hochzählen
+          countDictionary[word]++;
+        }
+        else
+        {
+          // wenn nicht, dann neu einfügen
+          countDictionary.Add(word, 1);
+        }
+
+      }
+
+      return countDictionary;
+    }
+  }
+}
---
+++ projects/Vokabulator.ConsoleApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using Vokabulator.BL;
+
+string content = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+
+// Datentyp; Bezeichnung; aufgerufene Klasse 
+var myWordCounter = new WordCounter(' ');
+
+// Datentyp; Bezeichnung; Ergebnis der Klasse; Funktion; Objekt
+Dictionary<string, int> words = myWordCounter.Count(content);
+
+foreach (KeyValuePair<string, int> pair in words)
+{
+  Console.WriteLine($"{pair.Key} kommt {pair.Value} Mal im Text vor.");
+}
+
+
+
+//Kaiser cäsar = new Kaiser("Julius");
+//Mensch derLange = new Mensch("Jan");
+
+//derLange.Alter
+
+//var eagle = new President("Joe Biden");
+//Console.WriteLine(eagle.VizePresigent.Name) -> Camilla Harris
+
+//var tvGröße = new Größe(140, 80);
+//int zoll = tvGröße.Zoll;
+//Console.WriteLine(zoll);
