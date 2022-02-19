0780153810a2353ec1bd2ab1adc469cc0c2a5752
Jan Kästner <jan.kaestner@outtlook.com>
2022-02-19 14:26:48 +0100

    .

--- projects/Vokabulator.ConsoleApplication/Program.cs
+++ projects/Vokabulator.ConsoleApplication/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Vokabulator.BL;
 
-string content = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+string content = System.IO.File.ReadAllText(@"C:\Users\JanKästner\Desktop\Textfile\file.txt");
 
 // Datentyp; Bezeichnung; aufgerufene Klasse 
 var myWordCounter = new WordCounter(' ');
